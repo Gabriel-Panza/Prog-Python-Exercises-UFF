ca6237aa9b3a975d3946d4e2525af7a8d385d6f5
Gabriel Panza <gabrieljpanza@gmail.com>
2023-06-01 16:32:53 -0300

    Update heart_interface.py

--- Interface/heart_interface.py
+++ Interface/heart_interface.py
@@ -1,12 +1,30 @@
-from turtle import *
+import turtle
 
-color("red")
-begin_fill()
-pensize(3)
-left(50)
-forward(133)
-circle(50,200)
-right(140)
-circle(50,200)
-forward(133)
-end_fill()
+# Configuração inicial da tartaruga
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("red")
+t.fillcolor("red")
+t.speed(2)
+
+# Desenhar o coração
+t.hideturtle()
+t.begin_fill()
+t.left(140)
+t.forward(180)
+t.circle(-90, 200)
+t.setheading(60)
+t.circle(-90, 200)
+t.forward(180)
+t.end_fill()
+
+# Escrever a string no centro do coração
+t.penup()
+t.goto(0, 0)
+t.setheading(0)
+t.color("white")
+t.setposition(0,125)
+t.write("Luisa", align="center", font=("Arial", 20, "bold"))
+
+# Manter a janela aberta
+turtle.done()
