ebf78c55db91577b6289208a5ef266cee1b4c6f6
Gabriel Panza <110632615+Gabriel-Panza@users.noreply.github.com>
2023-06-07 22:57:29 -0300

    Update 2231-Temperatura_Lunar.py

--- BeeCrowd/2231-Temperatura_Lunar.py
+++ BeeCrowd/2231-Temperatura_Lunar.py
@@ -1,25 +1,26 @@
-def calculate_temperature_averages(temperatures, interval):
-    n = len(temperatures)
-    sum_interval = sum(temperatures[:interval])
-    averages = [sum_interval // interval]
-
-    for i in range(0, n - interval):
-        sum_interval += temperatures[i + interval] - temperatures[i]
-        averages.append(sum_interval // interval)
-
-    return averages
-
-N_Temperatures, M_intervals = map(int, input().split())
-cont = 1
-
-while N_Temperatures != 0 and M_intervals != 0:
-    temperatures = [int(input()) for i in range(N_Temperatures)]
-    
-    min_average, max_average = min(calculate_temperature_averages(temperatures, M_intervals)), max(calculate_temperature_averages(temperatures, M_intervals))
-    
-    print("Teste", cont)
-    print(min_average, max_average)
-    print()
+n, m = map(int,input().split())
+cont = 0
+while n != 0 and m != 0:
     cont += 1
+    temperature_sequence = [int(input()) for i in range(n)]
+
+    lista_soma = []
+    aux = 0
+    for i in range(n):
+        soma = temperature_sequence[i] + aux
+        aux = soma
+        lista_soma.append(soma)
+
+    menor = (lista_soma[m-1])/m
+    maior = (lista_soma[m-1])/m
+    for i in range(m,len(lista_soma),1):
+        if (lista_soma[i] - lista_soma[i-m])/m > maior:
+                maior = (lista_soma[i] - lista_soma[i-m])/m
+        if (lista_soma[i] - lista_soma[i-m])/m < menor:
+                menor = (lista_soma[i] - lista_soma[i-m])/m
+
+    print(f"Teste {cont}")
+    print(int(menor),int(maior))
+    print()
     
-    N_Temperatures, M_intervals = map(int, input().split())
+    n, m = map(int, input().split())
